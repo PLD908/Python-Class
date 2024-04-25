82c38fa340c34f21f98012d758131596e7a01434
PLD908 <oluyededivine@gmail.com>
2024-04-25 19:04:58 +0100

    updated page

--- Clever-Story.py
+++ Clever-Story.py
@@ -16,4 +16,4 @@ print(Verb.lower())
 print(Verb.lower())
 
 print("Your story is: ")
-print('The other morning, I was strolling through the park when suddenly, a [Adjective] [Animal] [Verb] past me. "[Exclamation]" I exclaimed. Without hesitation, I decided to whistle loudly. Surprisingly, that caught its attention, but not before it attempted to dart across the path in front of my friends.')
+print(f'The other morning, I was strolling through the park when suddenly, a {Adjective.lower()} {Animal.lower()} {Verb.lower()} past me. "{Exclamation.capitalize()}" I exclaimed. Without hesitation, I decided to whistle loudly. Surprisingly, that caught its attention, but not before it attempted to dart across the path in front of my friends.')
