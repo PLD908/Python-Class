9f43375b624cecc534ba61749446c0e6f2675a28
PLD908 <oluyededivine@gmail.com>
2024-05-30 20:34:24 +0100

    Added new file

---
+++ cse-110/life-expectancy.py
@@ -0,0 +1,16 @@
+# Asking user to enter year of interest
+year_of_interest = int(input('Enter the year of interest: '))
+expectancy = ""
+entity = ""
+year = ""
+
+# with open("life-expectancy.csv") as f:
+#     for line in f:
+#         each_line = line.strip().split(",")
+#         print(each_line)
+        # if year_of_interest > line[2]:
+        #     year = year_of_interest
+        #     entity = each_line[0]
+        #     expectancy = each_line[3]
+        #     print(f"The overall max life expectancy is: {expectancy} from {entity} in {year}")
+            
